aea8239ccaf222b48b6a37af7bd271c6a2c83636
Olivier Huin <olih@users.noreply.github.com>
2023-05-13 09:59:38 +0100

    Adds software health

---
+++ SOFTWARE-HEALTH.md
@@ -0,0 +1,92 @@
+
+# Software health
+
+## Team journey overview
+
+```mermaid
+journey
+    title My working day
+    architecture: 0: Team
+    brainstorming: 0: Team
+    code: 2: Team
+    deps: 0: Team
+    diagram: 1: Team
+    doc: 2: Team
+    ops: 0: Team
+    specs: 0: Team
+    test: 3: Team
+    ux: 0: Team
+
+```
+
+## Fine grain health table
+
+| Document | Score | Rank  |
+|----------| ------|-------|
+| architecture clear-source-of-truth |  | 0 |
+| architecture data-retention |  | 0 |
+| architecture degrades-gracefully |  | 0 |
+| architecture easy-to-understand |  | 0 |
+| architecture loosely-coupled |  | 0 |
+| architecture single-responsibility |  | 0 |
+| architecture well-defined-interface |  | 0 |
+| brainstorming drawing |  | 0 |
+| brainstorming ideation |  | 0 |
+| code generation | ✰ | 3 |
+| code linting |  | 1 |
+| code readibility |  | 0 |
+| code refactoring |  | 0 |
+| deps audit |  | 0 |
+| deps diagram |  | 0 |
+| deps update |  | 0 |
+| diagram architecture |  | 0 |
+| diagram class |  | 0 |
+| diagram dependencies | ✰ | 2 |
+| diagram flow |  | 0 |
+| diagram mindmap |  | 0 |
+| diagram sequence |  | 0 |
+| diagram user-journey |  | 0 |
+| doc accessibility-report |  | 0 |
+| doc developer-guide | ✰✰ | 4 |
+| doc formatting |  | 0 |
+| doc grammar |  | 0 |
+| doc privacy-report |  | 0 |
+| doc proofreading |  | 0 |
+| doc references-api | ✰ | 2 |
+| doc references-glossary |  | 0 |
+| doc references-schema |  | 0 |
+| doc release-notes |  | 0 |
+| doc security-report |  | 0 |
+| doc spell-checking |  | 0 |
+| doc support |  | 0 |
+| doc troubleshooting |  | 0 |
+| doc user-guide |  | 0 |
+| ops alerting |  | 0 |
+| ops backup |  | 0 |
+| ops deployment |  | 0 |
+| ops diagnosis |  | 0 |
+| ops infrastructure-as-code |  | 0 |
+| ops monitoring |  | 0 |
+| ops rollback |  | 0 |
+| specs business-cases |  | 0 |
+| specs cost-benefit-analysis |  | 0 |
+| specs interviews |  | 0 |
+| specs risk-assessment |  | 0 |
+| specs roadmap |  | 0 |
+| specs surveys |  | 0 |
+| specs user-stories |  | 0 |
+| test accessibility |  | 0 |
+| test browser |  | 0 |
+| test device |  | 0 |
+| test end-to-end |  | 0 |
+| test functional |  | 0 |
+| test integration |  | 0 |
+| test platform |  | 0 |
+| test regression | ✰✰ | 6 |
+| test unit | ✰ | 3 |
+| ux information-architecture |  | 0 |
+| ux personas |  | 0 |
+| ux prototyping |  | 0 |
+| ux user-flows |  | 0 |
+| ux user-research |  | 0 |
+| ux wireframes |  | 0 |
--- software-health/sofware-health-summary.mjs
+++ software-health/sofware-health-summary.mjs
@@ -1,7 +1,195 @@
 #!/usr/bin/env zx
+const healthFiles = await glob('software-health/*.software-health.yaml');
 
-const healthFiles = await glob('software-health/*.software-health.yaml')
+const docName = {
+  code: ['readibility', 'refactoring', 'linting', 'generation'],
+  deps: ['update', 'audit', 'diagram'],
+  doc: [
+    'user-guide',
+    'developer-guide',
+    'references-api',
+    'references-schema',
+    'references-glossary',
+    'troubleshooting',
+    'release-notes',
+    'formatting',
+    'spell-checking',
+    'grammar',
+    'proofreading',
+    'accessibility-report',
+    'security-report',
+    'privacy-report',
+    'support',
+  ],
+  diagram: [
+    'architecture',
+    'class',
+    'sequence',
+    'flow',
+    'user-journey',
+    'mindmap',
+    'dependencies',
+  ],
+  test: [
+    'unit',
+    'regression',
+    'functional',
+    'integration',
+    'end-to-end',
+    'platform',
+    'browser',
+    'device',
+    'accessibility',
+  ],
+  specs: [
+    'business-cases',
+    'user-stories',
+    'cost-benefit-analysis',
+    'risk-assessment',
+    'roadmap',
+    'interviews',
+    'surveys',
+  ],
+  ux: [
+    'user-research',
+    'personas',
+    'information-architecture',
+    'user-flows',
+    'wireframes',
+    'prototyping',
+  ],
+  brainstorming: ['ideation', 'drawing'],
+  ops: [
+    'deployment',
+    'monitoring',
+    'rollback',
+    'alerting',
+    'backup',
+    'infrastructure-as-code',
+    'diagnosis',
+  ],
+  architecture: [
+    'single-responsibility',
+    'well-defined-interface',
+    'degrades-gracefully',
+    'loosely-coupled',
+    'easy-to-understand',
+    'clear-source-of-truth',
+    'data-retention',
+  ],
+};
+
+const scores = {};
+const roughScoreToRank = (roughScore) => {
+  let rank = 1;
+  if (roughScore.automation === 'automatic') {
+    rank += 1;
+  }
+  if (roughScore.deployment === 'ci') {
+    rank += 1;
+  }
+  if (roughScore.performance === 'milli-seconds') {
+    rank += 1;
+  }
+  if (roughScore.ai === 'full') {
+    rank += 1;
+  }
+  return rank;
+};
+
+const initScores = () => {
+  for (const rootKey of Object.keys(docName)) {
+    const rootContent = docName[rootKey];
+    for (const childKey of rootContent) {
+      const roughScore = rootContent[childKey];
+      const scoreKey = `${rootKey} ${childKey}`;
+      scores[scoreKey] = 0;
+    }
+  }
+};
+
+const contentToScores = (jsonContent) => {
+  for (const rootKey of Object.keys(jsonContent)) {
+    if ((rootKey === 'title') | (rootKey === 'github')) {
+      continue;
+    }
+    const rootContent = jsonContent[rootKey];
+    for (const childKey of Object.keys(rootContent)) {
+      const roughScore = rootContent[childKey];
+      const scoreKey = `${rootKey} ${childKey}`;
+      const previousScore =
+        scores[scoreKey] === undefined ? 0 : scores[scoreKey];
+      const newScore = previousScore + roughScoreToRank(roughScore);
+      scores[scoreKey] = newScore;
+    }
+  }
+};
+initScores();
 for (const healthFile of healthFiles) {
-  const content = await fs.readFile(healthFile, { encoding: 'utf8'})
-  console.log(YAML.parse(content))
+  const content = await fs.readFile(healthFile, { encoding: 'utf8' });
+  contentToScores(YAML.parse(content));
 }
+
+const scoreToStars = (score) => {
+  if (score === 0) {
+    return '';
+  }
+
+  const logScore = Math.ceil(Math.log(score) / Math.log(3));
+  return '✰'.repeat(logScore);
+};
+const scoreRows = Object.entries(scores).sort();
+
+const summariseScores = () => {
+  const summaryScoreCounting = {};
+  for (const [scoreKey, score] of scoreRows) {
+    const prefix = scoreKey.split(' ')[0];
+    const previous = summaryScoreCounting[prefix];
+    const newScore = previous === undefined ? score : previous + score;
+    summaryScoreCounting[prefix] = newScore;
+  }
+  return Object.entries(summaryScoreCounting).sort();
+};
+const summaryScoreRows = summariseScores();
+
+const scoreToSmiley = (score) => {
+  if (score === 0) {
+    return 0;
+  }
+
+  const logScore = Math.ceil(Math.log(score) / Math.log(3));
+  return logScore;
+};
+
+const scoreTable = scoreRows
+  .map(
+    (keyScore) =>
+      `| ${keyScore[0]} | ${scoreToStars(keyScore[1])} | ${keyScore[1]} |`
+  )
+  .join('\n');
+
+const scoreUserJourney = summaryScoreRows
+  .map((keyScore) => `    ${keyScore[0]}: ${scoreToSmiley(keyScore[1])}: Team`)
+  .join('\n');
+
+const codeMarker = (tag) => '```' + tag;
+
+const mdReport = `
+# Software health
+
+## Team journey overview
+
+${codeMarker('mermaid')}
+journey
+    title My working day
+${scoreUserJourney}
+
+${codeMarker('')}
+
+## Fine grain health table
+
+| Document | Score | Rank  |
+|----------| ------|-------|
+${scoreTable}
+`;
+await fs.writeFile('SOFTWARE-HEALTH.md', mdReport, { encoding: 'utf8' });
