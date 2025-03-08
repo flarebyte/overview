a5175edc4fb6698044142d947bb07b2f5ff974b5
Olivier Huin <olih@users.noreply.github.com>
2025-03-08 19:34:13 +0000

    Save stats to file

--- inventory/code-base-analysis.mjs
+++ inventory/code-base-analysis.mjs
@@ -14,68 +14,77 @@ import {
   getFieldLength,
 } from './utility.mjs';
 
-const topic = 'npm-package';
-const npmFolder = `/tmp/overview/${topic}`;
-const npmPackageRepos = await getRepos(topic);
-const numberOfDaysSinceCreation = npmPackageRepos.map(
-  (repo) => repo.numberOfDaysSinceCreation
-);
-const numberOfDaysSincePush = npmPackageRepos.map(
-  (repo) => repo.numberOfDaysSincePush
-);
-const numberOfDaysOfActivity = npmPackageRepos.map(
-  (repo) => repo.numberOfDaysOfActivity
-);
-const sizes = npmPackageRepos.map((repo) => repo.size);
+/** Clone all the github projects for given topic*/
+const processRepositoriesByTopic = async (topic, options) => {
+  const npmFolder = `/tmp/overview/${topic}`;
+  const npmPackageRepos = await getRepos(topic);
+  const numberOfDaysSinceCreation = npmPackageRepos.map(
+    (repo) => repo.numberOfDaysSinceCreation
+  );
+  const numberOfDaysSincePush = npmPackageRepos.map(
+    (repo) => repo.numberOfDaysSincePush
+  );
+  const numberOfDaysOfActivity = npmPackageRepos.map(
+    (repo) => repo.numberOfDaysOfActivity
+  );
+  const sizes = npmPackageRepos.map((repo) => repo.size);
 
-// const folder = await resetTempFolder('npm-package');
-// console.log(`- Reset folder ${folder}`);
+  if (options.clone) {
+    await resetTempFolder('npm-package');
+    console.log(`- Reset folder ${folder}`);
 
-//await cloneFlarebyteRepositories(folder, npmPackageRepos);
+    await cloneFlarebyteRepositories(folder, npmPackageRepos);
+  }
+  const trivyFsJSON = await runTrivyFs(npmFolder);
+  const trivyFsData = trivyFsSummary(trivyFsJSON);
 
-const trivyFsJSON = await runTrivyFs(npmFolder);
-const trivyFsData = trivyFsSummary(trivyFsJSON);
-console.log(trivyFsData);
+  const countOfProjects = 10;
+  const sccJson = await runScc(npmFolder);
+  const sccColumns = convertToIndexedColumnsFormat(simplifyScc(sccJson));
+  const rowCount = getFieldLength(sccColumns.Name);
+  const sccColumnMerged = {
+    ...createObjectField('Date', getISODateString(), rowCount),
+    ...createObjectField('Projects', countOfProjects, rowCount),
+    ...createObjectField('Category', topic, rowCount),
+    ...createObjectField(
+      'numberOfDaysSinceCreation',
+      numberOfDaysSinceCreation,
+      rowCount
+    ),
+    ...createObjectField(
+      'numberOfDaysSincePush',
+      numberOfDaysSincePush,
+      rowCount
+    ),
+    ...createObjectField(
+      'numberOfDaysOfActivity',
+      numberOfDaysOfActivity,
+      rowCount
+    ),
+    ...createObjectField('sizes', sizes, rowCount),
+    ...createObjectField(
+      'severity_high',
+      trivyFsData?.severity_high?.length || 0,
+      rowCount
+    ),
+    ...createObjectField(
+      'severity_medium',
+      trivyFsData?.severity_medium?.length || 0,
+      rowCount
+    ),
+    ...createObjectField(
+      'severity_low',
+      trivyFsData?.severity_low?.length || 0,
+      rowCount
+    ),
+    ...sccColumns,
+  };
 
-const countOfProjects = 10;
-const sccJson = await runScc(npmFolder);
-const sccColumns = convertToIndexedColumnsFormat(simplifyScc(sccJson));
-const rowCount = getFieldLength(sccColumns.Name);
-const sccColumnMerged = {
-  ...createObjectField('Date', getISODateString(), rowCount),
-  ...createObjectField('Projects', countOfProjects, rowCount),
-  ...createObjectField('Category', topic, rowCount),
-  ...createObjectField(
-    'numberOfDaysSinceCreation',
-    numberOfDaysSinceCreation,
-    rowCount
-  ),
-  ...createObjectField(
-    'numberOfDaysSincePush',
-    numberOfDaysSincePush,
-    rowCount
-  ),
-  ...createObjectField(
-    'numberOfDaysOfActivity',
-    numberOfDaysOfActivity,
-    rowCount
-  ),
-  ...createObjectField('sizes', sizes, rowCount),
-  ...createObjectField(
-    'severity_high',
-    trivyFsData?.severity_high?.length || 0,
-    rowCount
-  ),
-  ...createObjectField(
-    'severity_medium',
-    trivyFsData?.severity_medium?.length || 0,
-    rowCount
-  ),
-  ...createObjectField(
-    'severity_low',
-    trivyFsData?.severity_low?.length || 0,
-    rowCount
-  ),
-  ...sccColumns,
+  cd(process.env.PWD);
+  await fs.writeJson(
+    `./data/${topic}-${getISODateString()}.json`,
+    sccColumnMerged
+  );
 };
-console.log(sccColumnMerged);
+
+await processRepositoriesByTopic('npm-package', { clone: false });
